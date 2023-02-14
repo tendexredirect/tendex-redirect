cb6b7a213b836fb1bcec61ee05dfe3cd83d01fbc
Personal <logoscrappertwo@gmail.com>
2023-02-14 23:58:17 +0300

    fix

--- index.js
+++ index.js
@@ -8,28 +8,29 @@ const app = express();
 
 app.listen(port, () => {
     console.log("getStarted", new Date());
-   
+
 });
 
 app.get("/short/:short", function (req, res) {
     axios
-    .get("https://api-tendex.de/short/"+req.params.short)
-    .then((result) => {
-        res.redirect(result.request.res.responseUrl)
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .get("https://api-tendex.de/short/" + req.params.short)
+        .then((result) => {
+            res.redirect(result.request.res.responseUrl)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 app.get("/unsubscribe", function (req, res) {
-    let params=req.url.split('?')[1]
+    let params = req.url.split('?')[1]
     axios
-    .get("https://api-tendex.de/api/v1/blacklist/unsubscribe?"+params)
-    .then((result) => {
-        res.send(result.data)
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .get("https://api-tendex.de/api/v1/blacklist/unsubscribe?" + params)
+        .then((result) => {
+            res.redirect('https://tendex.net')
+
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
